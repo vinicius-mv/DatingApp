d5434ba64ce112a43f64f24623e77e4deb6320df
Vinicius Martins <viniciusmv91@gmail.com>
2022-11-12 22:59:59 -0300

    Start to implement Angular template forms

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -6,9 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  model: any = {}
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  login(): void {
+    console.log(this.model);
   }
 }
