0470e2aceea1db881020f3e5a3fe5acea635fd22
Vinicius Martins <viniciusmv91@gmail.com>
2022-11-26 22:31:37 -0300

    client - add validation errors

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -31,7 +31,7 @@ export class ErrorInterceptor implements HttpInterceptor {
                     modelStateErrors.push(error.error.errors[key])
                   }
                 }
-                throw modelStateErrors;
+                throw modelStateErrors.flat();
               } else {
                 this.toastr.error("Bad Request", error.status.toString())
               }
