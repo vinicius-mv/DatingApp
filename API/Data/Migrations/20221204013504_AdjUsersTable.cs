ea23a77b7641bcd7610b1d43bf0b7a4639111de4
Vinicius Martins <viniciusmv91@gmail.com>
2022-12-03 22:39:23 -0300

    client - update users controller

---
+++ API/Data/Migrations/20221204013504_AdjUsersTable.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class AdjUsersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Intrests",
+                table: "Users",
+                newName: "Interests");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Interests",
+                table: "Users",
+                newName: "Intrests");
+        }
+    }
+}
