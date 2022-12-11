19bd44ee2d312f004b37ab55fad4c6d0ce372e47
Vinicius Martins <viniciusmv91@gmail.com>
2022-12-10 21:21:36 -0300

    client - add member interface

---
+++ client/src/app/_models/member.ts
@@ -0,0 +1,18 @@
+import { Photo } from "./photo";
+
+export interface Member {
+    id: number;
+    userName: string;
+    photoUrl: string;
+    age: number;
+    knowAs?: any;
+    created: Date;
+    lastActive: Date;
+    gender: string;
+    introduction: string;
+    lookingFor: string;
+    interests?: any;
+    city: string;
+    country: string;
+    photos: Photo[];
+}
--- client/src/app/_models/user.ts
+++ client/src/app/_models/user.ts
@@ -2,4 +2,3 @@ export interface User {
     username: string;
     token: string;
 }
-
