861e02e2671baea0b490424789b7cfe6e245e1a6
Vinicius Martins <viniciusmv91@gmail.com>
2022-12-28 23:30:37 -0300

    client - add jwt token in an interceptor service

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -13,24 +13,10 @@ export class MembersService {
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users', this.getHttpOptions());
+    return this.http.get<Member[]>(this.baseUrl + 'users');
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + 'users/' + username, this.getHttpOptions())
+    return this.http.get<Member>(this.baseUrl + 'users/' + username)
   }
-
-  getHttpOptions() {
-    const userString = localStorage.getItem('user');
-    if (!userString) return;
-    const user: User = JSON.parse(userString);
-
-    return {
-      headers: new HttpHeaders({
-        Authorization: 'Bearer ' + user.token
-      })
-    }
-  }
-
-
 }
