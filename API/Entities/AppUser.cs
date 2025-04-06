a8371ded5933a02ab86b37a18f63060b95badfbb
Vinícius Vieira <viniciusmv91@gmail.com>
2025-04-06 16:46:28 -0300

    Add PasswordHash and PasswordSalt properties to AppUser and update migration

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,5 +4,6 @@ public class AppUser
 {
     public int Id { get; set; }
     public required string Username { get; set; }
-
+    public required byte[] PasswordHash { get; set; }
+    public required byte[] PasswordSalt { get; set; }
 }
