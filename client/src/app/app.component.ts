b4e6a2426eeec40ba4d861723dc662ae3b220c09
Vinícius Vieira <viniciusmv91@gmail.com>
2025-04-06 22:01:30 -0300

    Add HomeComponent with registration toggle and integrate into app layout

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,10 +4,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
 import { User } from './_models/users';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
