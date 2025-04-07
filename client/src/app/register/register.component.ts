6e8207bb60404bf9d46be789ac501d91106955c7
Vinícius Vieira <viniciusmv91@gmail.com>
2025-04-06 22:14:52 -0300

    Add RegisterComponent with registration form (ngForm Template) and integrate into HomeComponent

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  imports: [FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  model: any = {};
+
+  register() {
+    console.log(this.model);
+  }
+
+  cancel() {
+    console.log('cancelled');
+  }
+}
