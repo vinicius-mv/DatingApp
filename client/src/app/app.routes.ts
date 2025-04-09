e5958c4d9cccba7c1b09febdd4c2e5e86ec545ef
Vinícius Vieira <viniciusmv91@gmail.com>
2025-04-08 23:17:27 -0300

    Refactor routing structure to group member-related routes under auth guard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -8,9 +8,17 @@ import { authGuard } from './_guards/auth.guard';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'members', component: MemberListComponent, canActivate: [authGuard] },
-    { path: 'member/:id', component: MemberDetailComponent },
-    { path: 'lists', component: ListsComponent },
-    { path: 'messages', component: MessagesComponent },
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            { path: 'members', component: MemberListComponent },
+            { path: 'member/:id', component: MemberDetailComponent },
+            { path: 'lists', component: ListsComponent },
+            { path: 'messages', component: MessagesComponent },
+        ]
+    },
+
     { path: '**', component: HomeComponent, pathMatch: 'full' },
 ];
