f9ec002ee132f019fac128955efc776fbc4f15b8
Vinícius Vieira <viniciusmv91@gmail.com>
2025-04-10 22:57:49 -0300

    Add TestErrorsComponent and update routing for error testing

---
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+
+@Component({
+  selector: 'app-test-errors',
+  imports: [],
+  templateUrl: './test-errors.component.html',
+  styleUrl: './test-errors.component.css'
+})
+export class TestErrorsComponent {
+  baseUrl = 'https://localhost:5001/api/'
+  private http = inject(HttpClient);
+
+
+  get400Error() {
+    this.http.get(this.baseUrl + 'buggy/bad-request').subscribe({
+      next: response => console.log(response),
+      error: err => console.log(err)
+    });
+  }
+
+  get401Error() {
+    this.http.get(this.baseUrl + 'buggy/auth').subscribe({
+      next: response => console.log(response),
+      error: err => console.log(err)
+    });
+  }
+
+  get404Error() {
+    this.http.get(this.baseUrl + 'buggy/not-found').subscribe({
+      next: response => console.log(response),
+      error: err => console.log(err)
+    });
+  }
+
+  get500Error() {
+    this.http.get(this.baseUrl + 'buggy/server-error').subscribe({
+      next: response => console.log(response),
+      error: err => console.log(err)
+    });
+  }
+
+  get400ValidationError() {
+    this.http.post(this.baseUrl + 'account/register', {}).subscribe({
+      next: response => console.log(response),
+      error: err => console.log(err)
+    });
+  }
+}
