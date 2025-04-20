f23d1c2c34ebcb8123a46014cf21413c181a29c0
Vinícius Vieira <viniciusmv91@gmail.com>
2025-04-20 16:56:40 -0300

    Update Photo model to add required one to many relationship between AppUser and Photo in EF

--- API/Entities/Photo.cs
+++ API/Entities/Photo.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API.Entities;
 
+[Table("photos")]
 public class Photo
 {
     public int Id { get; set; }
     public required string Url { get; set; }
     public bool IsMain { get; set; }
     public string? PublicId { get; set; }
+
+    // Navigation properties (required one to many)
+    public int AppUserId { get; set; }
+
+    public AppUser AppUser { get; set; } = null!;
 }
