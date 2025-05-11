3d7f7f1b5382ee94eaf15e455cee9b6f4a5a5de7
Vinícius Vieira <viniciusmv91@gmail.com>
2025-05-11 14:02:57 -0300

    Enhance MemberDetailComponent with detailed member information display, including location, age, last active, and member since. Update styles for improved layout and add tab functionality for interests and messages.

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -2,10 +2,11 @@ import { Component, inject, OnInit } from '@angular/core';
 import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @Component({
   selector: 'app-member-detail',
-  imports: [],
+  imports: [TabsModule],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
