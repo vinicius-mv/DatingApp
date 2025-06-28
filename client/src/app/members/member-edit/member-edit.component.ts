0c22bb712c3a4e6bdb574d1cf64d6697a1bb9241
vinicius-mv <viniciusmv91@gmail.com>
2025-06-28 00:34:35 -0300

    Implement member profile editing functionality with form validation; add styles and layout for improved user experience.

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,18 +1,23 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { Member } from '../../_models/member';
 import { AccountService } from '../../_services/account.service';
 import { MembersService } from '../../_services/members.service';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-member-edit',
-  imports: [],
+  imports: [TabsModule, FormsModule],
   templateUrl: './member-edit.component.html',
   styleUrl: './member-edit.component.css'
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm?: NgForm;
   member?: Member;
   private accountService = inject(AccountService);
   private memberService = inject(MembersService);
+  private toaster = inject(ToastrService);
 
   ngOnInit(): void {
     this.loadMember();
@@ -26,5 +31,11 @@ export class MemberEditComponent implements OnInit {
     this.memberService.getMember(user.username).subscribe({
       next: members => this.member = members
     });
+  }
+
+  updateMember() {
+    console.log(this.member);
+    this.toaster.success('Profile updated successfully');
+    this.editForm?.reset(this.member);
   }
 }
