6c369519542f4052fdc9d1558fb06c2ce4640e0f
vinicius-mv <viniciusmv91@gmail.com>
2025-06-27 23:38:28 -0300

    Add MemberEditComponent for editing member profiles; update routing and navigation for profile editing.

---
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,30 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { Member } from '../../_models/member';
+import { AccountService } from '../../_services/account.service';
+import { MembersService } from '../../_services/members.service';
+
+@Component({
+  selector: 'app-member-edit',
+  imports: [],
+  templateUrl: './member-edit.component.html',
+  styleUrl: './member-edit.component.css'
+})
+export class MemberEditComponent implements OnInit {
+  member?: Member;
+  private accountService = inject(AccountService);
+  private memberService = inject(MembersService);
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+
+  loadMember() {
+    const user = this.accountService.currentUser();
+    if (!user) return;
+
+    this.memberService.getMember(user.username).subscribe({
+      next: members => this.member = members
+    });
+  }
+}
