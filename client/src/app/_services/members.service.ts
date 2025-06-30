c9c6a838af29c7849d7541f04e08c2d97718fa78
vinicius-mv <viniciusmv91@gmail.com>
2025-06-29 22:26:18 -0300

    Refactor MembersService use signals for state management and update member list in MemberListComponent

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Member } from '../_models/member';
+import { of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +10,25 @@ import { Member } from '../_models/member';
 export class MembersService {
   private http = inject(HttpClient);
   baseUrl = environment.apiUrl;
+  members = signal<Member[]>([]);
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    return this.http.get<Member[]>(this.baseUrl + 'users').subscribe({
+      next: members => this.members.set(members)
+    });
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + 'users/' + username)
+    let member = this.members().find(m => m.username === username);
+    if (member) return of(member);
+
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
   updateMember(member: Member) {
-    return this.http.put(this.baseUrl + 'users', member);
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      tap(() => {
+        this.members.update(members => members.map(m => m.username === member.username ? member : m))
+      }));
   }
 }
